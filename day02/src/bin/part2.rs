e19ae04e1350b3afb8792cd22fe088834a0f9bea
David Jahng <dave.jahng@gmail.com>
2024-12-23 13:35:47 -0700

    Day 2, Part 2

--- day02/src/bin/part2.rs
+++ day02/src/bin/part2.rs
@@ -1,3 +1,48 @@
+use std::{env, fs};
+
+fn solve(input: String) -> u32 {
+    let mut length = 0;
+
+    for line in input.lines() {
+        let line = line.trim();
+        let mut parts = line
+            .split("x")
+            .into_iter()
+            .filter_map(|x| x.parse::<u32>().ok())
+            .collect::<Vec<_>>();
+        parts.sort_unstable();
+
+        length += 2 * parts[0] + 2 * parts[1];
+        length += parts[0] * parts[1] * parts[2];
+    }
+
+    length
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<_>>();
+    let path = args
+        .get(1)
+        .unwrap_or(&"input/input.txt".to_string())
+        .to_string();
+    let input = fs::read_to_string(path).expect("to read file");
+    let length = solve(input);
+
+    println!("Length: {length}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_solves() {
+        let input = "2x3x4".to_string();
+        let length = solve(input);
+        assert_eq!(length, 34);
+
+        let input = "1x1x10".to_string();
+        let length = solve(input);
+        assert_eq!(length, 14);
+    }
 }
